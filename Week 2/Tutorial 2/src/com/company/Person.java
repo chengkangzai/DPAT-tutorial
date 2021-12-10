a04c349cf3058849a0ed7f9f1ecd0d89a69b665f
chengkangzai <pycck@hotmail.com>
2021-12-10 10:07:17 +0800

    make person abstract class and ask seller to inherit it

--- Week 2/Tutorial 2/src/com/company/Person.java
+++ Week 2/Tutorial 2/src/com/company/Person.java
@@ -2,7 +2,7 @@ package com.company;
 
 //2.	Create a Person class
 //•	personName and personID as String
-public class Person {
+abstract public class Person {
     String personName;
     String personID;
 }
--- Week 2/Tutorial 2/src/com/company/Seller.java
+++ Week 2/Tutorial 2/src/com/company/Seller.java
@@ -1,7 +1,7 @@
 package com.company;
 
 // creating a Seller class to sell the product.  The Seller class will implement the ProductInterface
-public class Seller implements ProductInterface {
+public class Seller extends Person implements ProductInterface {
     // creating a method to sell the product
     public void sellProduct() {
         // write your code here
